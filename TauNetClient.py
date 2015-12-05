6fe34448b06297ce08edcf8025e2d33f769ab062
Micah Burnett <persistentlobster@gmail.com>
2015-12-05 12:08:44 -0800

    Imlemented client/server body to send and receive plaintext on local network

--- TauNetClient.py
+++ TauNetClient.py
@@ -1,2 +1,26 @@
 # Micah Burnett 11/30/15 CS303
 # Client Application for TauNet v1.0
+
+import sys
+import socket
+
+
+#connect to local host. TODO: change...
+HOST = '127.0.0.1'   
+PORT = 6283
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+srvAddr = socket.gethostbyname(HOST)
+sock.connect((srvAddr, PORT))     
+sock.send("Yo dawg")
+
+"""while True:
+    print("Establishing connection to server...")
+    conn, addr = sock.accept()
+
+    
+    msg = conn.recv(1024)
+    print("Received msg:", msg)
+
+    conn.close()"""
--- TauNetServer.py
+++ TauNetServer.py
@@ -1,2 +1,42 @@
 # Micah Burnett 11/30/15 CS300
 # This is the server-side application for TauNet v1.0
+
+import sys
+import socket
+
+HOST = ''           # local ip
+PORT = 6283
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+    print("Socket creation failed, Error:", str(msg[0]), ", Msg:", msg[1])
+    sys.exit()
+
+print("Created socket")
+
+
+try:
+    sock.bind((HOST, PORT))
+except socket.error as msg:
+    print("Bind to socket failed:", str(msg[0]), ", Msg:", msg[1])
+
+print("Bind successful")
+
+
+sock.listen(5)
+print("Listening for requests...")
+
+#conn, addr = sock.accept()
+#msg = conn.recv(1024)
+#print(msg)
+
+while True:
+    conn, addr = sock.accept()
+    msg = conn.recv(1024)
+    print(msg)
+    print ("Connected with " + addr[0] + ":" + str(addr[1]))
+
+sock.close()
+
+
